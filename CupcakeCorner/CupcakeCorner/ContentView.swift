6e0f8961de6a05bab95b3c153be6a7f78b56c17d
galih <samudragalih94@gmail.com>
2024-07-19 15:44:01 +0700

    feat(UI): create simple validation form

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    @State private var name: String = ""
+    @State private var email: String = ""
     var body: some View {
-        Text("Hello world")
+        Form {
+            Section {
+                TextField("Username", text: $name)
+                TextField("Email", text: $email)
+            }
+            Section {
+                Button("Confirm") {
+                    print("Confimation process...")
+                }
+            }
+            .disabled(name.isEmpty || email.isEmpty)
+        }
     }
 }
 
