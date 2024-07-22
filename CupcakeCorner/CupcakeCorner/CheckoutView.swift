8b09f0381e3ffc9272caa19015ff1602294f4c6e
galih <samudragalih94@gmail.com>
2024-07-22 13:44:24 +0700

    feat(UI): show cost and image with simple UI on CheckoutView

--- CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -7,10 +7,36 @@
 
 import SwiftUI
 
+struct CustomImageView: View {
+    let urlString: String
+    var body: some View {
+        AsyncImage(url: URL(string: urlString),scale: 3) { image in
+            image
+                .resizable()
+                .scaledToFit()
+        } placeholder: {
+            ProgressView()
+        }
+    }
+}
+
 struct CheckoutView: View {
     var order: Order
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                CustomImageView(urlString: "https://hws.dev/img/cupcakes@3x.jpg")
+                    .frame(height: 233)
+                Text("Your total is \(order.cost, format: .currency(code: "USD"))")
+                    .font(.title)
+                Button("Place Order") {
+                    
+                }
+                .padding()
+            }
+        }
+        .navigationTitle("Check Out")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
