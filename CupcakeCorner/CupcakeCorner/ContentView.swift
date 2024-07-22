311f4bbcead28e0096a81cfd8fbd9507fce3e050
galih <samudragalih94@gmail.com>
2024-07-22 09:11:52 +0700

    feat(sensor): specify an intense collision between two heavy objects

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
         Button("Tap count \(counter)") {
             counter += 1
         }
-        .sensoryFeedback(.impact(flexibility: .soft, intensity: 0.5), trigger: counter)
+        .sensoryFeedback(.impact(weight: .heavy, intensity: 1), trigger: counter)
     }
 }
 
