89a5db4581204ae59789890f55ca39d0f4c82957
galih <samudragalih94@gmail.com>
2024-07-22 09:26:31 +0700

    feat(Heptics): example custom haptics

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -6,15 +6,50 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
 struct ContentView: View {
+    @State private var engine: CHHapticEngine?
     @State private var counter = 0
     
     var body: some View {
-        Button("Tap count \(counter)") {
-            counter += 1
+        Button("tap me", action: complexSuccess)
+            .onAppear(perform: prepareHeptics)
+    }
+    
+    private func prepareHeptics() {
+        /// make sure that device support haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("Error start CHHapticEngine - \(error)")
         }
-        .sensoryFeedback(.impact(weight: .heavy, intensity: 1), trigger: counter)
+    }
+    
+    private func complexSuccess() {
+        /// make sure that device support haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+        
+        /// create one intense, sharp tap
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+        
+        /// convert those events into a pattern and play it immediately
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Error play pattern - \(error)")
+        }
+        
     }
 }
 
