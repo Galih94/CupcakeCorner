b20a6c56a56518f4a66acc726d3a3afddc92b337
galih <samudragalih94@gmail.com>
2024-07-22 09:04:43 +0700

    clean code

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -9,14 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Button("Encode Taylor", action: encodeTaylor)
-        
-    }
-    
-    private func encodeTaylor() {
-        let data = try! JSONEncoder().encode(User())
-        let str = String(decoding: data, as: UTF8.self)
-        print(str)
+        Text("Hello world")
     }
 }
 
