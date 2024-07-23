f4bc878de64608b5bcf611b2e8c69ec9f6f189e4
galih <samudragalih94@gmail.com>
2024-07-23 13:45:17 +0700

    feat(Validation): Improve the validation to make sure a string of pure whitespace is invalid.

--- CupcakeCorner/CupcakeCorner/Model.swift
+++ CupcakeCorner/CupcakeCorner/Model.swift
@@ -60,7 +60,10 @@ class Order: Codable {
     var zip = ""
     
     var hasValidAddress: Bool {
-        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty || name.trimmingCharacters(in: .whitespaces).isEmpty ||
+            streetAddress.trimmingCharacters(in: .whitespaces).isEmpty ||
+            city.trimmingCharacters(in: .whitespaces).isEmpty ||
+            zip.trimmingCharacters(in: .whitespaces).isEmpty {
             return false
         }
         return true
